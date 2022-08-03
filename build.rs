4a97cce99fd45bfeddb5bc714f9cf1ce6db668bb
Brad Hesson <brad.hesson@outlook.com>
2022-08-02 23:10:19 -0600

    automatically apply riscv target fix

--- build.rs
+++ build.rs
@@ -1,3 +1,4 @@
+#![feature(iter_intersperse)]
 use std::env;
 use std::fs;
 use std::path::PathBuf;
@@ -15,6 +16,7 @@ fn main() {
 
 fn compile_c_code(file: PathBuf, search_paths: Vec<PathBuf>) {
     let mut build = cc::Build::new();
+    build.compiler("clang");
     for dir in search_paths.clone() {
         for file in fs::read_dir(dir).unwrap() {
             let path = file.unwrap().path();
@@ -27,8 +29,13 @@ fn compile_c_code(file: PathBuf, search_paths: Vec<PathBuf>) {
     for sp in search_paths {
         build.include(sp);
     }
+    if let Some(target) = get_riscv_target_fixed() {
+        build.target(target.as_str());
+    }
     build.flag_if_supported("-Wno-builtin-declaration-mismatch");
-    build.flag_if_supported("-Wno-implicit-function-declaration");
+    build.flag_if_supported("-Wno-missing-declarations");
+    build.flag("-Wno-#pragma-messages");
+    build.flag("-Wno-implicit-function-declaration");
     build.flag_if_supported("-Wno-format");
     build.compile(file.file_stem().unwrap().to_str().unwrap());
 }
@@ -41,8 +48,10 @@ fn run_bindgen(file: PathBuf, search_paths: Vec<PathBuf>) {
         .parse_callbacks(Box::new(bindgen::CargoCallbacks))
         .ctypes_prefix("cty")
         .header(file.to_str().unwrap())
-        .use_core()
-        .clang_arg("--target=riscv32-esp-espidf");
+        .use_core();
+    if let Some(target) = get_riscv_target_fixed() {
+        builder = builder.clang_arg(format!("--target={}", target));
+    }
     for sp in search_paths {
         builder = builder.clang_arg(format!("-I{}", sp.display()));
     }
@@ -52,4 +61,14 @@ fn run_bindgen(file: PathBuf, search_paths: Vec<PathBuf>) {
     bindings
         .write_to_file(out_path.join("bindings.rs"))
         .expect("Couldn't write bindings!");
+}
+
+fn get_riscv_target_fixed() -> Option<String> {
+    let target = env::var("TARGET").expect("Target environment variable should be set");
+    let mut triple = target.split("-").collect::<Vec<_>>();
+    if triple[0].starts_with("riscv") {
+        triple[0] = &triple[0][..7];
+        return Some(triple.into_iter().intersperse("-").collect());
+    }
+    None
 }
